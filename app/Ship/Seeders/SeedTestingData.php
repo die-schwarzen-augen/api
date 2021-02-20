518e7e5c313e30bee2829cf997ccd764f9b307b0
Sebastian Weckend <sebastian.weckend@posteo.de>
2021-02-20 20:20:34 +0100

    Add admin test seeder

--- app/Ship/Seeders/SeedTestingData.php
+++ app/Ship/Seeders/SeedTestingData.php
@@ -2,7 +2,10 @@
 
 namespace App\Ship\Seeders;
 
+use Apiato\Core\Foundation\Facades\Apiato;
+use App\Containers\User\Models\User;
 use App\Ship\Parents\Seeders\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * Class SeedTestingData
@@ -19,7 +22,35 @@ class SeedTestingData extends Seeder
      */
     public function run()
     {
-        // Create Testing data for live tests
+        $this->admins();
+    }
+
+    protected function admins()
+    {
+      $admins = [
+        'spokk' => [
+          'name' => 'Spokk',
+          'email' => 'spokk@posteo.de',
+        ],
+        'sebastian' => [
+          'name' => 'Sebastian',
+          'email' => 'sebastian@borbarad-projekt.de',
+        ],
+      ];
+
+      $hash = Hash::make('test');
+
+      foreach ($admins as $admin) {
+        if (!User::where('email', $admin['email'])->exists()) {
+          $user = User::create([
+              'is_client' => false,
+              'email'     => $admin['email'],
+              'password'  => $hash,
+              'name'      => $admin['name'],
+              'confirmed' => true,
+          ])->assignRole(Apiato::call('Authorization@FindRoleTask', ['admin']));
+        }
+      }
     }
 
 }
